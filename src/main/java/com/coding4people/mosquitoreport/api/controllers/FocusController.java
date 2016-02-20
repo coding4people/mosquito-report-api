81bd6fd7b4a1a251c0e64616dcbb037bda911a22
Rogério Yokomizo <me@ro.ger.io>
2016-02-20 17:13:24 +0100

    Save focus model

--- src/main/java/com/coding4people/mosquitoreport/api/controllers/FocusController.java
+++ src/main/java/com/coding4people/mosquitoreport/api/controllers/FocusController.java
@@ -1,5 +1,8 @@
 package com.coding4people.mosquitoreport.api.controllers;
 
+import java.util.Date;
+
+import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
@@ -7,13 +10,26 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.coding4people.mosquitoreport.api.models.Focus;
+import com.coding4people.mosquitoreport.api.repositories.FocusRepository;
+
 @Path("/focus")
 public class FocusController {
+    @Inject FocusRepository focusRepository;
+    
     @POST
     @Consumes("application/json")
     @Produces("application/json;charset=UTF-8")
-    public String post(@Valid FocusPostInput focus) {
-        return "\"" + focus.getLatitude() + "\"";
+    public Focus post(@Valid FocusPostInput input) {
+        Focus focus = new Focus();
+        focus.setGuid(java.util.UUID.randomUUID().toString());
+        focus.setLatitude(input.getLatitude());
+        focus.setLongitude(input.getLongitude());
+        focus.setCreateAt(Long.toString(new Date().getTime()));
+        
+        focusRepository.save(focus);
+        
+        return focus;
     }
     
     public static class FocusPostInput {
