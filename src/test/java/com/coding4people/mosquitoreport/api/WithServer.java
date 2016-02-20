1e0771d58f787c02543d00d7bf87841db992f643
Rogério Yokomizo <me@ro.ger.io>
2016-02-20 20:21:11 +0100

    Mock attributes on configuration in order to build generic factories

--- src/test/java/com/coding4people/mosquitoreport/api/WithServer.java
+++ src/test/java/com/coding4people/mosquitoreport/api/WithServer.java
@@ -1,15 +1,35 @@
 package com.coding4people.mosquitoreport.api;
 
-import org.glassfish.jersey.CommonProperties;
-import org.glassfish.jersey.jackson.internal.FilteringJacksonJaxbJsonProvider;
-import org.glassfish.jersey.jackson.internal.JacksonFilteringFeature;
+import java.lang.reflect.Field;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-public class WithServer extends JerseyTest {
+abstract public class WithServer extends JerseyTest {
     @Override
     protected ResourceConfig configure() {
-        return new ResourceConfig().property(CommonProperties.FEATURE_AUTO_DISCOVERY_DISABLE, true)
-                .register(JacksonFilteringFeature.class).register(FilteringJacksonJaxbJsonProvider.class);
+        final WithServer t = this;
+        final Field[] fields = getClass().getDeclaredFields();
+        
+        MockitoAnnotations.initMocks(t);
+        
+        return new ResourceConfig().register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                for (Field field : fields) {
+                    if (field.isAnnotationPresent(Mock.class)) {
+                        try {
+                            field.setAccessible(true);
+                            bindFactory(new InstanceFactory<Object>(field.get(t))).to(field.getType());
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
     }
 }
