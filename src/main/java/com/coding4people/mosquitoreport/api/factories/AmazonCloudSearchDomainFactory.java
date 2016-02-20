2c53a0665725866c02fbeef18f1e59b803bdd2a1
Rogério Yokomizo <me@ro.ger.io>
2016-02-21 00:25:56 +0100

    AmazonCloudSearchDomainFactory look for `focus` endpoint

--- src/main/java/com/coding4people/mosquitoreport/api/factories/AmazonCloudSearchDomainFactory.java
+++ src/main/java/com/coding4people/mosquitoreport/api/factories/AmazonCloudSearchDomainFactory.java
@@ -1,15 +1,32 @@
 package com.coding4people.mosquitoreport.api.factories;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.InternalServerErrorException;
+
 import org.glassfish.hk2.api.Factory;
 
 import com.amazonaws.services.cloudsearchdomain.AmazonCloudSearchDomain;
 import com.amazonaws.services.cloudsearchdomain.AmazonCloudSearchDomainClient;
+import com.amazonaws.services.cloudsearchv2.AmazonCloudSearch;
+import com.amazonaws.services.cloudsearchv2.model.DescribeDomainsRequest;
+import com.amazonaws.services.cloudsearchv2.model.DomainStatus;
 
 public class AmazonCloudSearchDomainFactory implements Factory<AmazonCloudSearchDomain> {
     AmazonCloudSearchDomain client;
 
-    public AmazonCloudSearchDomainFactory() {
+    @Inject
+    public AmazonCloudSearchDomainFactory(AmazonCloudSearch amazonCloudSearch) {
+        DescribeDomainsRequest describeDomainsRequest = new DescribeDomainsRequest().withDomainNames("focus");
+        List<DomainStatus> list = amazonCloudSearch.describeDomains(describeDomainsRequest).getDomainStatusList();
+
+        if (list.isEmpty()) {
+            throw new InternalServerErrorException("Could not find CloudSearch domain");
+        }
+
         client = new AmazonCloudSearchDomainClient();
+        client.setEndpoint(list.get(0).getSearchService().getEndpoint());
     }
 
     @Override
