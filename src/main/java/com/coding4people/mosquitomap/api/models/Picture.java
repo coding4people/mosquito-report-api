3751a19bc108c3910c9929a2d58e419f85b3b275
Rogério Yokomizo <me@ro.ger.io>
2016-02-20 13:02:24 +0100

    Create Picture model

---
+++ src/main/java/com/coding4people/mosquitomap/api/models/Picture.java
@@ -0,0 +1,23 @@
+package com.coding4people.mosquitomap.api.models;
+
+public class Picture {
+    private String focusGuid;
+    
+    private String createAt;
+
+    public String getFocusGuid() {
+        return focusGuid;
+    }
+
+    public void setFocusGuid(String focusGuid) {
+        this.focusGuid = focusGuid;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+}
