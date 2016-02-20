08c61996400ece51a29a6d9a966b76df084846e1
Rogério Yokomizo <me@ro.ger.io>
2016-02-20 15:44:33 +0100

    Create base test class

---
+++ src/test/java/com/coding4people/mosquitomap/api/WithServer.java
@@ -0,0 +1,15 @@
+package com.coding4people.mosquitomap.api;
+
+import org.glassfish.jersey.CommonProperties;
+import org.glassfish.jersey.jackson.internal.FilteringJacksonJaxbJsonProvider;
+import org.glassfish.jersey.jackson.internal.JacksonFilteringFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+public class WithServer extends JerseyTest {
+    @Override
+    protected ResourceConfig configure() {
+        return new ResourceConfig().property(CommonProperties.FEATURE_AUTO_DISCOVERY_DISABLE, true)
+                .register(JacksonFilteringFeature.class).register(FilteringJacksonJaxbJsonProvider.class);
+    }
+}
