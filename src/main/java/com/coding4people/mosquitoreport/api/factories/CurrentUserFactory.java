0a749547201e241fb6b5cc47b72ac9ba6f7695d8
Rogério Yokomizo <me@ro.ger.io>
2016-02-21 21:21:32 +0100

    Add CurrentUserFactory

---
+++ src/main/java/com/coding4people/mosquitoreport/api/factories/CurrentUserFactory.java
@@ -0,0 +1,45 @@
+package com.coding4people.mosquitoreport.api.factories;
+
+import java.util.Base64;
+
+import javax.inject.Inject;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.container.ContainerRequestContext;
+
+import org.glassfish.hk2.api.Factory;
+import org.glassfish.jersey.process.internal.RequestScoped;
+
+import com.coding4people.mosquitoreport.api.models.User;
+import com.coding4people.mosquitoreport.api.repositories.UserRepository;
+
+@RequestScoped
+public class CurrentUserFactory implements Factory<User> {
+    User user;
+
+    @Inject
+    public CurrentUserFactory(UserRepository userRepository, ContainerRequestContext requestContext) {
+        String authorization = requestContext.getHeaderString("Authorization");
+
+        if (authorization == null || authorization.isEmpty() || !authorization.startsWith("Token ")) {
+            throw new ForbiddenException("Missing 'Authorization' HTTP header");
+        }
+
+        // TODO implement decent authorization algorithm
+        final String guid = new String(Base64.getDecoder().decode(authorization.substring(6)));
+
+        user = userRepository.load(guid);
+
+        if (user == null) {
+            throw new ForbiddenException("User not authenticate");
+        }
+    }
+
+    @Override
+    public void dispose(User user) {
+    }
+
+    @Override
+    public User provide() {
+        return user;
+    }
+}
--- src/main/java/com/coding4people/mosquitoreport/api/factories/FactoryBinder.java
+++ src/main/java/com/coding4people/mosquitoreport/api/factories/FactoryBinder.java
@@ -3,6 +3,7 @@ package com.coding4people.mosquitoreport.api.factories;
 import javax.inject.Singleton;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.process.internal.RequestScoped;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.services.cloudsearchv2.AmazonCloudSearch;
@@ -11,6 +12,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.coding4people.mosquitoreport.api.Env;
+import com.coding4people.mosquitoreport.api.models.User;
 
 public class FactoryBinder extends AbstractBinder {
     @Override
@@ -18,6 +20,7 @@ public class FactoryBinder extends AbstractBinder {
         bindFactory(AmazonCloudSearchFactory.class).to(AmazonCloudSearch.class).in(Singleton.class);
         bindFactory(AmazonDynamoDBFactory.class).to(AmazonDynamoDB.class).in(Singleton.class);
         bindFactory(AmazonS3ClientFactory.class).to(AmazonS3Client.class).in(Singleton.class);
+        bindFactory(CurrentUserFactory.class).to(User.class).in(RequestScoped.class);
         bindFactory(DynamoDBConfigurationFactory.class).to(ClientConfiguration.class).in(Singleton.class);
         bindFactory(DynamoDBFactory.class).to(DynamoDB.class).in(Singleton.class);
         bindFactory(DynamoDBMapperFactory.class).to(DynamoDBMapper.class).in(Singleton.class);
