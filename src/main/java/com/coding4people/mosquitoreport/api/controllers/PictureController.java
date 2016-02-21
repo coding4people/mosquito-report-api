372a5418438eeaf162f72d26647a3b6eb334632d
Rogério Yokomizo <me@ro.ger.io>
2016-02-21 23:35:32 +0100

    Protect PictureController

--- src/main/java/com/coding4people/mosquitoreport/api/controllers/PictureController.java
+++ src/main/java/com/coding4people/mosquitoreport/api/controllers/PictureController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.UUID;
 
 import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,7 +17,10 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import com.coding4people.mosquitoreport.api.buckets.PictureBucket;
+import com.coding4people.mosquitoreport.api.models.Focus;
 import com.coding4people.mosquitoreport.api.models.Picture;
+import com.coding4people.mosquitoreport.api.models.User;
+import com.coding4people.mosquitoreport.api.repositories.FocusRepository;
 import com.coding4people.mosquitoreport.api.repositories.PictureRepository;
 
 @Path("/picture")
@@ -24,7 +28,11 @@ public class PictureController {
     @Inject
     PictureRepository pictureRepository;
     @Inject
+    FocusRepository focusRepository;
+    @Inject
     PictureBucket pictureBucket;
+    @Inject
+    User user;
 
     @POST
     @Consumes({ MediaType.MULTIPART_FORM_DATA })
@@ -32,6 +40,12 @@ public class PictureController {
     @Path("/focus/{focusguid}")
     public Object post(@PathParam("focusguid") String focusguid, @FormDataParam("file") InputStream fileInputStream,
             @FormDataParam("file") FormDataContentDisposition fileDisposition) {
+        Focus focus = focusRepository.loadOrNotFound(focusguid);
+        
+        if (user.getGuid().equals(focus.getAuthoruserguid())) {
+            throw new BadRequestException("User does not own this item");
+        }
+        
         Picture picture = new Picture();
         picture.setGuid(UUID.randomUUID().toString());
         picture.setFilename(fileDisposition.getFileName());
