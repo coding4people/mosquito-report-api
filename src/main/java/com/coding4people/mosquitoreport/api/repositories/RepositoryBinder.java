686a722a26190a5e9de1eca07f73f02e8ac2d33d
Rogério Yokomizo <me@ro.ger.io>
2016-02-21 11:23:58 +0100

    Bind new repositories

--- src/main/java/com/coding4people/mosquitoreport/api/repositories/RepositoryBinder.java
+++ src/main/java/com/coding4people/mosquitoreport/api/repositories/RepositoryBinder.java
@@ -5,7 +5,11 @@ import org.glassfish.hk2.utilities.binding.AbstractBinder;
 public class RepositoryBinder extends AbstractBinder {
     @Override
     protected void configure() {
+        bind(EmailRepository.class).to(EmailRepository.class);
         bind(FocusRepository.class).to(FocusRepository.class);
+        bind(PasswordResetTokenRepository.class).to(PasswordResetTokenRepository.class);
         bind(PictureRepository.class).to(PictureRepository.class);
+        bind(ThumbsUpRepository.class).to(ThumbsUpRepository.class);
+        bind(UserRepository.class).to(UserRepository.class);
     }
 }
