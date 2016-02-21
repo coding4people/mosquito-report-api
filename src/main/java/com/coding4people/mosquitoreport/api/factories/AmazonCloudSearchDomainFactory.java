25f12ffecd485370509aefba4c9efec23ec64277
Rogério Yokomizo <me@ro.ger.io>
2016-02-21 02:56:27 +0100

    AmazonCloudSearchDomainFactory is a singleton

--- src/main/java/com/coding4people/mosquitoreport/api/factories/AmazonCloudSearchDomainFactory.java
+++ src/main/java/com/coding4people/mosquitoreport/api/factories/AmazonCloudSearchDomainFactory.java
@@ -3,6 +3,7 @@ package com.coding4people.mosquitoreport.api.factories;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.ws.rs.InternalServerErrorException;
 
 import org.glassfish.hk2.api.Factory;
@@ -13,6 +14,7 @@ import com.amazonaws.services.cloudsearchv2.AmazonCloudSearch;
 import com.amazonaws.services.cloudsearchv2.model.DescribeDomainsRequest;
 import com.amazonaws.services.cloudsearchv2.model.DomainStatus;
 
+@Singleton
 public class AmazonCloudSearchDomainFactory implements Factory<AmazonCloudSearchDomain> {
     AmazonCloudSearchDomain client;
 
