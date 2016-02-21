6ec7a8ec498abf69671b6f70d5e723ee595f08b3
Rogério Yokomizo <me@ro.ger.io>
2016-02-21 03:29:48 +0100

    Create singletons on app scope

--- src/main/java/com/coding4people/mosquitoreport/api/factories/FactoryBinder.java
+++ src/main/java/com/coding4people/mosquitoreport/api/factories/FactoryBinder.java
@@ -1,9 +1,10 @@
 package com.coding4people.mosquitoreport.api.factories;
 
+import javax.inject.Singleton;
+
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 import com.amazonaws.ClientConfiguration;
-import com.amazonaws.services.cloudsearchdomain.AmazonCloudSearchDomain;
 import com.amazonaws.services.cloudsearchv2.AmazonCloudSearch;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -13,12 +14,11 @@ import com.coding4people.mosquitoreport.api.Env;
 public class FactoryBinder extends AbstractBinder {
     @Override
     protected void configure() {
-        bindFactory(AmazonCloudSearchDomainFactory.class).to(AmazonCloudSearchDomain.class);
-        bindFactory(AmazonCloudSearchFactory.class).to(AmazonCloudSearch.class);
-        bindFactory(AmazonDynamoDBFactory.class).to(AmazonDynamoDB.class);
-        bindFactory(DynamoDBConfigurationFactory.class).to(ClientConfiguration.class);
-        bindFactory(DynamoDBFactory.class).to(DynamoDB.class);
-        bindFactory(DynamoDBMapperFactory.class).to(DynamoDBMapper.class);
-        bindFactory(EnvFactory.class).to(Env.class);
+        bindFactory(AmazonCloudSearchFactory.class).to(AmazonCloudSearch.class).in(Singleton.class);
+        bindFactory(AmazonDynamoDBFactory.class).to(AmazonDynamoDB.class).in(Singleton.class);
+        bindFactory(DynamoDBConfigurationFactory.class).to(ClientConfiguration.class).in(Singleton.class);
+        bindFactory(DynamoDBFactory.class).to(DynamoDB.class).in(Singleton.class);
+        bindFactory(DynamoDBMapperFactory.class).to(DynamoDBMapper.class).in(Singleton.class);
+        bindFactory(EnvFactory.class).to(Env.class).in(Singleton.class);
     }
 }
--- src/main/java/com/coding4people/mosquitoreport/api/indexers/IndexerBinder.java
+++ src/main/java/com/coding4people/mosquitoreport/api/indexers/IndexerBinder.java
@@ -1,10 +1,12 @@
 package com.coding4people.mosquitoreport.api.indexers;
 
+import javax.inject.Singleton;
+
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 public class IndexerBinder extends AbstractBinder {
     @Override
     protected void configure() {
-        bind(FocusIndexer.class).to(FocusIndexer.class);
+        bind(FocusIndexer.class).to(FocusIndexer.class).in(Singleton.class);
     }
 }
