6a91b65bac6d23b1de3997cda0b7e3237aeab488
Rogério Yokomizo <me@ro.ger.io>
2016-02-21 11:42:38 +0100

    Add password hash methods

--- src/main/java/com/coding4people/mosquitoreport/api/models/Email.java
+++ src/main/java/com/coding4people/mosquitoreport/api/models/Email.java
@@ -1,5 +1,7 @@
 package com.coding4people.mosquitoreport.api.models;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
@@ -37,5 +39,17 @@ public class Email {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    public static String encryptPassword(String rawPassword) {
+        return BCrypt.hashpw(rawPassword, BCrypt.gensalt(12));
+    }
+    
+    public static boolean checkPassword(String rawPassword, String password) {
+        if(null == password || !password.startsWith("$2a$")) {
+            throw new IllegalArgumentException("Invalid hash provided for comparison");
+        }
+        
+        return BCrypt.checkpw(rawPassword, password);
     }
 }
