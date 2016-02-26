b6e4aba0f75e103bb799cc1e342592dc4fab9ea7
Rogério da Silva Yokomizo <me@ro.ger.io>
2016-02-26 01:51:18 +0100

    Test repository loaders

--- src/test/java/com/coding4people/mosquitoreport/api/repositories/RepositoryTest.java
+++ src/test/java/com/coding4people/mosquitoreport/api/repositories/RepositoryTest.java
@@ -1,7 +1,10 @@
 package com.coding4people.mosquitoreport.api.repositories;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import javax.ws.rs.NotFoundException;
 
 import org.junit.Test;
 import org.mockito.Mock;
@@ -22,28 +25,85 @@ public class RepositoryTest extends WithService {
 
     @Mock
     DynamoDB dynamoDB;
-    
+
     @Mock
     Table table;
 
     @Test
     public void testSave() {
         CreateTableRequest request = new CreateTableRequest();
-        
+
         when(mapper.generateCreateTableRequest(Model.class)).thenReturn(request);
         when(dynamoDB.createTable(request)).thenReturn(table);
-        
+
         ModelRepository repository = getService(ModelRepository.class);
-        
+
         Model model = new Model();
-        
+
         repository.save(model);
-        
+
         verify(mapper).save(model);
     }
+
+    @Test
+    public void testLoadByHash() {
+        CreateTableRequest request = new CreateTableRequest();
+
+        when(mapper.generateCreateTableRequest(Model.class)).thenReturn(request);
+        when(dynamoDB.createTable(request)).thenReturn(table);
+
+        ModelRepository repository = getService(ModelRepository.class);
+
+        repository.load("hash");
+
+        verify(mapper).load(Model.class, "hash");
+    }
+
+    @Test
+    public void testLoadByHashAndRange() {
+        CreateTableRequest request = new CreateTableRequest();
+
+        when(mapper.generateCreateTableRequest(Model.class)).thenReturn(request);
+        when(dynamoDB.createTable(request)).thenReturn(table);
+
+        ModelRepository repository = getService(ModelRepository.class);
+
+        repository.load("hash", "range");
+
+        verify(mapper).load(Model.class, "hash", "range");
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testLoadOrNotFound() {
+        CreateTableRequest request = new CreateTableRequest();
+
+        when(mapper.generateCreateTableRequest(Model.class)).thenReturn(request);
+        when(dynamoDB.createTable(request)).thenReturn(table);
+
+        ModelRepository repository = getService(ModelRepository.class);
+
+        repository.loadOrNotFound("hash");
+    }
     
-    public static class Model {}
-    
+    @Test
+    public void testLoadOrNotFound200() {
+        CreateTableRequest request = new CreateTableRequest();
+        Model model = new Model();
+
+        when(mapper.generateCreateTableRequest(Model.class)).thenReturn(request);
+        when(dynamoDB.createTable(request)).thenReturn(table);
+        when(mapper.load(Model.class, "hash")).thenReturn(model);
+
+        ModelRepository repository = getService(ModelRepository.class);
+
+        assertEquals(model, repository.loadOrNotFound("hash"));
+        
+        verify(mapper).load(Model.class, "hash");
+    }
+
+    public static class Model {
+    }
+
     public static class ModelRepository extends Repository<Model> {
         @Override
         protected Class<Model> getType() {
