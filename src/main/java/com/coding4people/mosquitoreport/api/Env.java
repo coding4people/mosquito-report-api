92fd74b4c859a4cb54a47e193c974f0913546477
Rogério Yokomizo <me@ro.ger.io>
2016-03-06 22:23:24 +0100

    Add properties to Env

--- src/main/java/com/coding4people/mosquitoreport/api/Env.java
+++ src/main/java/com/coding4people/mosquitoreport/api/Env.java
@@ -1,9 +1,21 @@
 package com.coding4people.mosquitoreport.api;
 
+import java.util.Map;
+
+import jersey.repackaged.com.google.common.collect.Maps;
+
 public class Env {
+    public final static Env instance = new Env();
+    
+    private Map<String, String> properties = Maps.newHashMap();
     
     public String get(String key) {
-        return System.getenv(key);
+        return properties.getOrDefault(key, System.getenv(key));
     }
-
+    
+    public Env register(String key, String value) {
+        properties.put(key, value);
+        
+        return this;
+    }
 }
